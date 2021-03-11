c5fd9aea6be17ce7603276382c06e11aba5fac36
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-03-11 13:44:29 -0600

    html code update

--- Develop/script.js
+++ Develop/script.js
@@ -5,5 +5,6 @@ var timeDisplayEl = $('#time-display');
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
-  console.log(rightNow);
 }
+displayTime();
+
